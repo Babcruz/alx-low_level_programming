0d90c3694f575950fce2d30f5d445d5e9a74e9b7
Babcruz <bakirita1993@gmail.com>
2022-03-23 14:07:54 -0700

    Updated

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ m = (n % 10);
 
 if (m > 5)
 {
-printf("Last digit of %d is %d is greater than 5\n", n, n % 10);
+printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 }
 else if (m == 0)
 {
