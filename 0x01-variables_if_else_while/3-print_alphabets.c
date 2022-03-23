7060fc7d8bae5606aee155401f3a3bf7e62f5804
Babcruz <bakirita1993@gmail.com>
2022-03-23 09:51:48 -0700

    ALPHABETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0
+ */
+
+int main(void)
+{
+char x, c;
+
+for (x = 'a'; x <= 'z'; ++x)
+{
+putchar(x);
+}
+for(c ='A'; c <= 'Z' ; ++c)
+{
+putchar(c);
+}
+{
+	putchar('\n');
+}
+return (0);
+}
