0123a1523b76eb448cd54b0ebffc565a0fb46bbf
Babcruz <bakirita1993@gmail.com>
2022-03-23 11:18:55 -0700

    Base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0
+ */
+
+int main(void)
+{
+int x, c;
+
+for (x = '0'; x <= '9'; ++x)
+{
+putchar(x);
+}
+for (c = 'a'; c <= 'f' ; ++c)
+{
+putchar(c);
+}
+{
+	putchar('\n');
+}
+return (0);
+}
